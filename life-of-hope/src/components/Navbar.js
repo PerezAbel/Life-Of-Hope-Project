060caba203b71dbb325b1cbc317bff03ae7b3d8c
Perez Kazungu <abelkazungu2020@gmail.com>
2025-06-14 14:48:39 +0300

    Update

---
+++ life-of-hope/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'; 
+
+
+const NavBar = () => {
+    return (   
+        <div className="NavBar">  
+
+        {/*NavBar  
+            Logo  
+            AboutUs 
+            Our Stories    
+            More(dropdown)    
+               Our Services  
+               Updates  
+               Gallery     
+               Transformimg Girls  
+               Covid-19 
+               Our Challenges          
+            ContactUs 
+            Donate
+        */  }
+        
+        </div>   
+    );
+};
+
+export default NavBar;
---
+++ life-of-hope/src/components/TopButton.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+
+
+const TopButton = () => {
+    return (   
+        <div className="NavBar">  
+
+        {/*NavBar   
+          About Our Webiste  
+          Usefull links  
+          Important Links  
+          Address
+          Email 
+          Phone  
+          Cportight 
+          Socials
+         */  }
+        </div>   
+    );
+};
+
+export default TopButton;
